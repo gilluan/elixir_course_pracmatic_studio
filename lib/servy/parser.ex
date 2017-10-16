8fb5065f671347e38a387b669fac4b8018045113
Gilluan Formiga <gilluan@hotmail.com.br>
2017-10-16 00:54:21 -0200

    add post request

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,20 +1,35 @@
 defmodule Servy.Parser do
 	
-	alias Servy.Conv
+  alias Servy.Conv
 
-	def parse(request) do
-	   
-	    [method, path, _] =
-	      request
-	      |> String.split("\n")
-	      |> List.first
+  def parse(request) do
+    [top, params_string] = String.split(request, "\n\n")
 
-	      |> String.split(" ")
+    [request_line | header_line] = String.split(top, "\n")
 
-	    %Conv{
-	    	method: method,
-	      	path: path     
-	    }
- 
-  	end
+    [method, path, _] = String.split(request_line, " ")
+
+      
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    params = params_string |> parse_params
+
+    %Conv{
+    	method: method,
+      	path: path,
+	params: params
+    }
+  end
+
+  def parse_params(params) do
+    params
+    |> String.trim
+    |> URI.decode_query
+  end
+  
+
 end
